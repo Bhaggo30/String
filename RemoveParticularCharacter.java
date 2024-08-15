c4906256938c559ac147ae84385944448a38ee78
bhagyashree <bhaggo30july@gmail.com>
2024-08-15 23:32:45 +0530

    remove the particular charcter from the string

--- RemoveParticularCharacter.java
+++ RemoveParticularCharacter.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class RemoveParticularCharacter {
-}
+public class RemoveParticularCharacter {
+
+        public static void main(String[] args) {
+            String originalString = "Hello World";
+            char characterToRemove = 'o';
+
+
+            String resultString = "";
+
+
+            for (int i = 0; i < originalString.length(); i++) {
+
+                char currentChar = originalString.charAt(i);
+
+
+                if (currentChar != characterToRemove) {
+                    resultString += currentChar;
+                }
+            }
+
+
+            System.out.println("Original String: " + originalString);
+            System.out.println("Modified String: " + resultString);
+        }
+    }
+
+
